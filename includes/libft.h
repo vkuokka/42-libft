6ef1069f1a853bc254484c082176497397aa3ddc
vkuokka <vkuokka@student.hive.fi>
2020-09-24 17:10:42 +0300

    One include to rule them all.

---
+++ includes/libft.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vkuokka <vkuokka@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/24 10:42:50 by vkuokka           #+#    #+#             */
+/*   Updated: 2020/09/27 10:43:00 by vkuokka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBRARY_H
+# define LIBRARY_H
+
+# include "arrays.h"
+# include "ctypes.h"
+# include "dlst.h"
+# include "ft_printf.h"
+# include "get_next_line.h"
+# include "lst.h"
+# include "memory.h"
+# include "numbers.h"
+# include "print.h"
+# include "strings.h"
+# include "swap.h"
+
+#endif
